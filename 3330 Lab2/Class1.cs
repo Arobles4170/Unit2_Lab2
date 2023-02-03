1c408f6ff24fc80ec05d58068e1d8eed3c00deab
Angel Robles <a.robles4170@gmail.com>
2023-02-03 12:39:58 -0500

    first commit

---
+++ 3330 Lab2/Class1.cs
@@ -0,0 +1,23 @@
+﻿namespace Task5
+{
+public class Class1
+{
+    public static double Pow(double num1,double num2)
+    {
+            double result_one;
+            result_one = Math.Pow(num1, num2);
+
+            Console.WriteLine("{0} to the power of {1} is: {2}", num1, num2, Math.Pow(num1, num2));
+            return result_one;
+        }
+
+    public static double Pow1(double result_1, double num3)
+    {
+            double result_two;
+            result_two = Math.Pow(result_1, num3);
+
+            Console.WriteLine("{0} to the power of {1} is: {3} ", result_1, num3, Math.Pow(result_1, num3));
+            return result_two;
+        }
+}
+}
---
+++ Task 5 Test/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+
+namespace TestTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1, num2, num3, result_one, result_two;
+            
+            
+                Console.WriteLine("Please enter your first number: ");
+                num1 = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Please enter your second number: ");
+                num2 = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Please enter your third number: ");
+                num3 = Convert.ToDouble(Console.ReadLine());
+
+
+            result_one = Task5.Class1.Pow(num1, num2);
+            result_two = Task5.Class1.Pow(result_one, num3);
+
+
+        }
+    }
+}
